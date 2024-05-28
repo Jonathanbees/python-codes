a5ad0a8aea8aaacc6eff0d73ae66b1d975c0e10d
Jonathanbees <jonathanbetancurespinosa@gmail.com>
2024-05-28 00:33:48 -0500

    pyramid advances

--- Leetcode/pyramid.py
+++ Leetcode/pyramid.py
@@ -2,13 +2,19 @@ def draw_pyramid(n):
     height = 2*n
     width = (3*(n-1) + 1) + 3 + height - 1
     print(width)
+    for i in range(height, 0, -1):
+        if i == height-1:
+            print('\\' + "/" + "__")
+        else:
+            print("*"*width)
 
     #Nota reconocimiento patrón 2: (tener en cuenta que estoy contando de abajo para arriba la altura) 
     #imprimir _, esta se imprime a partir de la línea h hasta h=2
+    #otro patron: _ se garantiza que SIEMPRE estará antes de cerrar la pirámide \, y la cantidad de _ que se imprimen por linea (de abajo hacia arriba) es height sumando el _ adicional al lado de \
 
-    return '\n'.join([
-        " ./\\ ",
-       "\\/__\\"])
+    #return '\n'.join([
+    #    " ./\\ ",
+    #   "\\/__\\"])
 """
 print('''
     ./\   
@@ -17,4 +23,18 @@ print('''
  \/_|__|_\\
 '''.replace(' ', '*'))
 """
-draw_pyramid(4)
+    
+draw_pyramid(1)
+
+def generate_sequence_excluding_modulo(n, mod, exclude):
+    sequence = []
+    i = 0
+    while len(sequence) < n:
+        if i % mod != exclude:
+            sequence.append(i)
+        i += 1
+    return sequence
+
+n = 10  # Número de términos deseados
+sequence = generate_sequence_excluding_modulo(n, 6, 1)
+print(sequence)
