4961cb9391263bf8ac518d229489b064a395774a
Jonathanbees <jonathanbetancurespinosa@gmail.com>
2024-06-04 23:48:55 -0500

    almost finish kata extract_domain

--- Leetcode/Extract_Domain_name.py
+++ Leetcode/Extract_Domain_name.py
@@ -1,23 +1,23 @@
-def domain_name(url):
-    startdomain = url.find(".")
-    finaldomain = url.rfind(".")
-    substring = url[startdomain+1:finaldomain]
-
-
-
-    """
-    if startdomain == -1:
+def domain_name(url: str):
+    if "//www" in url or "www" in url:
         startdomain = url.find(".")
-        print(startdomain)
         finaldomain = url.rfind(".")
-        print(finaldomain)
         substring = url[startdomain+1:finaldomain]
+    elif "http://" in url or "https://":
+        startdomain = url.find("//")
+        #print(startdomain)
+        if startdomain == -1:
+            startdomain = url.find("//")
+            finaldomain = url.find(".")
+            substring = url[startdomain+2:finaldomain]
+            #print(substring)
+            return substring
+        else:
+            finaldomain = url.find(".")
+            substring = url[startdomain+2:finaldomain]
     else:
-        finaldomain = url.find(".")
-        substring = url[startdomain+2:finaldomain]
-
-    print(substring)
-    #return substring
-    """
-    print(substring)
-domain_name("http://www.codewars.com/kata/")
+        finaldomain = url.rfind(".")
+        substring = url[:finaldomain]
+    #print(substring)
+    return substring
+domain_name("https://youtube.com")
